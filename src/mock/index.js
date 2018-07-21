69e34c44054a1b560a7accbdcd008b84a80bc6dc
许忠亮 <geniusmickymouse@qq.com>
2018-07-21 21:25:39 +0800

    Initial commit

---
+++ src/mock/index.js
@@ -0,0 +1,26 @@
+import Mock from 'mockjs'
+import loginAPI from './login'
+
+// Mock.setup({
+//   timeout: '350-600'
+// })
+
+// 登录相关
+Mock.mock(/\/user\/login/, 'post', loginAPI.login)
+Mock.mock(/\/user\/logout/, 'post', loginAPI.logout)
+Mock.mock(/\/user\/info\.*/, 'get', loginAPI.getInfo)
+
+// 文章相关
+// Mock.mock(/\/article\/list/, 'get', articleAPI.getList)
+// Mock.mock(/\/article\/detail/, 'get', articleAPI.getArticle)
+// Mock.mock(/\/article\/pv/, 'get', articleAPI.getPv)
+// Mock.mock(/\/article\/create/, 'post', articleAPI.createArticle)
+// Mock.mock(/\/article\/update/, 'post', articleAPI.updateArticle)
+
+// 搜索相关
+// Mock.mock(/\/search\/user/, 'get', remoteSearchAPI.searchUser)
+
+// 账单相关
+// Mock.mock(/\/transaction\/list/, 'get', transactionAPI.getList)
+
+export default Mock
---
+++ src/mock/login.js
@@ -0,0 +1,44 @@
+import { param2Obj } from '@/utils'
+
+const userMap = {
+  admin: {
+    roles: ['admin'],
+    token: 'admin',
+    introduction: '我是超级管理员',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Super Admin'
+  },
+  editor: {
+    roles: ['editor'],
+    token: 'editor',
+    introduction: '我是编辑',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Normal Editor'
+  }
+}
+
+const resultModel = {
+  code: 200,
+  msg: ''
+}
+
+export default {
+  login: config => {
+    const { username } = JSON.parse(config.body)
+    resultModel['data'] = userMap[username]
+    return resultModel
+  },
+  getInfo: config => {
+    const { token } = param2Obj(config.url)
+    if (userMap[token]) {
+      resultModel['data'] = userMap[token]
+      return resultModel
+    } else {
+      return false
+    }
+  },
+  logout: () => {
+    resultModel['data'] = 'success'
+    return resultModel
+  }
+}
